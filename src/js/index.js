99a1eecb5288c8ea7eae6e671ac095623b0c5fde
Sereda Vladyslav <v.u.sereda@gmail.com>
2021-08-08 18:37:07 +0300

    completed task

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+let correctValueTime = (value) => {
+    return value < 10 ? '0' + value : value
+}
+
+class Clock {
+    constructor(element) {
+        let el = element
+        el.addEventListener('click', () => this.changeFormat())
+        this.render = function () {
+            this.date = new Date()
+            let hours = correctValueTime(this.date.getHours())
+            let minutes = correctValueTime(this.date.getMinutes())
+            let seconds = correctValueTime(this.date.getSeconds())
+            this.time = el.classList.contains('full') ? `${hours}:${minutes}:${seconds}` : `${hours}:${minutes}`
+            el.innerHTML = this.time
+            return this.time
+        }
+        this.changeFormat = function () {
+            el.classList.toggle('full')
+        }
+    }
+}
+
+let time = document.getElementById('time')
+let clock = new Clock(time);
+clock.render()
+setInterval(() => clock.render(), 1000);
